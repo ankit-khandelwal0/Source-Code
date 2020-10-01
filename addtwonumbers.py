5a87089883fffda47ffb9a8695bc306de76f0ffe
ankit-khandelwal0 <70423552+ankit-khandelwal0@users.noreply.github.com>
2020-10-01 11:01:47 +0530

    Add files via upload
    Add Python File

---
+++ addtwonumbers.py
@@ -0,0 +1,8 @@
+#WAP to add two numbers enter by the user
+#Author:Gautam Sharma
+#Date:25-09-2020
+print("*****Welcome To Python World*******")
+firstnumber= int(input("Enter the First Number="))
+secondnumber= int(input("Enter the Second Number="))
+sum=firstnumber+secondnumber
+print("Sum=",sum)
